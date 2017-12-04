c6063422ff821ca27deabc6efea286c30f46caa5
godric <godric.com@gmail.com>
2017-12-04 15:36:59 +0100

    Implement day 01 part 02

--- day_01.py
+++ day_01.py
@@ -1,15 +1,28 @@
 import sys
 
 
-def at_index(items, i):
-    return items[i % len(items)]
+def at_next(items, i):
+    return items[(i + 1) % len(items)]
 
 
 def solve_puzzle(input):
-    matched = [int(digit) for i, digit in enumerate(input) if digit == at_index(input, i+1)]
+    matched = [int(digit) for i, digit in enumerate(input) if digit == at_next(input, i)]
+    return sum(matched)
+
+
+def at_halfway(items, i):
+    half = int(len(items) / 2)
+    return items[(i + half) % len(items)]
+
+
+def solve_second_puzzle(input):
+    matched = [int(digit) for i, digit in enumerate(input) if digit == at_halfway(input, i)]
     return sum(matched)
 
 
 if __name__ == '__main__':
-    result = solve_puzzle(sys.argv[1])
+    if sys.argv[1] == '--part-two':
+        result = solve_second_puzzle(sys.argv[2])
+    else:
+        result = solve_puzzle(sys.argv[1])
     print(result)
--- test_day01.py
+++ test_day01.py
@@ -1,4 +1,4 @@
-from day_01 import solve_puzzle
+from day_01 import solve_puzzle, solve_second_puzzle
 
 
 def test_solve_correctly_the_povided_examples():
@@ -6,3 +6,11 @@ def test_solve_correctly_the_povided_examples():
     assert solve_puzzle('1111') == 4
     assert solve_puzzle('1234') == 0
     assert solve_puzzle('91212129') == 9
+
+
+def test_part_two_solves_correctly_the_povided_examples():
+    assert solve_second_puzzle('1212') == 6
+    assert solve_second_puzzle('1221') == 0
+    assert solve_second_puzzle('123425') == 4
+    assert solve_second_puzzle('123123') == 12
+    assert solve_second_puzzle('12131415') == 4
