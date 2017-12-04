9737f136b949355310774ad9fe30cca8cc630758
godric <godric.com@gmail.com>
2017-12-04 16:13:21 +0100

    Solve day 02 part 02

--- day_02.py
+++ day_02.py
@@ -10,8 +10,23 @@ def solve_puzzle(lines):
     return sum([line_checksum(line.strip()) for line in lines])
 
 
+def line_evenly_divisible_result(line):
+    values = [int(token) for token in line.split()]
+    for i in values:
+        for j in values:
+            if i > j and i % j == 0:
+                return i / j
+
+
+def solve_second_puzzle(lines):
+    return sum([line_evenly_divisible_result(line.strip()) for line in lines])
+
+
 if __name__ == '__main__':
     if sys.argv[1] == '--part-one':
         input = sys.stdin.readlines()
         result = solve_puzzle(input)
+    if sys.argv[1] == '--part-two':
+        input = sys.stdin.readlines()
+        result = solve_second_puzzle(input)
     print(result)
--- test_day02.py
+++ test_day02.py
@@ -1,4 +1,4 @@
-from day_02 import line_checksum, solve_puzzle
+from day_02 import line_checksum, solve_puzzle, line_evenly_divisible_result
 
 
 def test_should_compute_correctly_a_line_checksum():
@@ -14,3 +14,9 @@ def test_should_solve_correctly_the_input_checksum():
       '2 4 6 8',
     ]
     assert solve_puzzle(lines) == 18
+
+
+def test_line_evenly_divisible_result():
+    assert line_evenly_divisible_result('5 9 2 8') == 4
+    assert line_evenly_divisible_result('9 4 7 3') == 3
+    assert line_evenly_divisible_result('3 8 6 5') == 2
